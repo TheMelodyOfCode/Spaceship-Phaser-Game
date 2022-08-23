dc66fce4a967e8c438593d594eb5e1ee6722d31a
D.Cameron <lobbygow@posteo.de>
2022-08-23 16:33:05 +0200

    added menu items and menu events

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -6,13 +6,35 @@ class MenuScene extends BaseScene {
 
     constructor(config){
         super('MenuScene', config);
+        this.menu = [
+          {scene: 'PlayScene', text: 'Play'},
+          {scene: 'ScoreScene', text: 'Score'},
+          {scene: null, text: 'Exit'},
+        ]
     }
 
     create() {
       super.create();
-      this.scene.start('PlayScene'); 
+      this.createMenu(this.menu, (menuItem) => this.setupMenuEvents(menuItem));
+      // this.scene.start('PlayScene'); 
     }
 
+    setupMenuEvents(menuItem){
+      const textgameObject = menuItem.textGameObject;
+      textgameObject.setInteractive();
+
+      textgameObject.on('pointerover', () => {
+        textgameObject.setStyle({
+          fill: '#FF0'
+        });
+      })
+      textgameObject.on('pointerout', () => {
+        textgameObject.setStyle({
+          fill: '#FFF'
+        });
+      })
+    }
+    
     
 }
 
